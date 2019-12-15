2c06130ad287273731e4eecc2c50f15e00d23308
gakisan8273 <mint.daa.a2@gmail.com>
2019-12-15 17:45:28 +0900

    グーグルアドセンスのメタタグを設定

--- head.php
+++ head.php
@@ -35,4 +35,6 @@
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
   <title>Report Supporter for Twitter</title>
+
+  <script data-ad-client="ca-pub-8219787462267423" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
